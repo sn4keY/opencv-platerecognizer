87528b495833f89027a8e975c28673aab29d35ee
Norbert Neudert <norbertneudert@gmail.com>
2019-12-06 15:22:49 +0100

    Added GetGrayscaleImg function which creates a grayscale image from the given image

--- OpenCVPlateRecognizer/OpenCVPlateRecognizer/OpenCVPlateRecognizer.cpp
+++ OpenCVPlateRecognizer/OpenCVPlateRecognizer/OpenCVPlateRecognizer.cpp
@@ -1,27 +1,30 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
-int main(int argc, char** argv)
+const string FOLDER = "D:\\Source\\OpenCVPlateRecognizer\\audi\\";
+
+Mat GetGrayscaleImg(Mat defaultImg) {
+	Mat hsv;
+	cvtColor(defaultImg, hsv, COLOR_RGB2HSV);
+	Mat hsvSplit[3];
+	split(hsv, hsvSplit);
+	return hsvSplit[2];
+}
+
+int main()
 {
-	if (argc != 2)
-	{
-		cout << " Usage: " << argv[0] << " ImageToLoadAndDisplay" << endl;
-		return -1;
-	}
-	Mat image;
-	image = imread(argv[1], IMREAD_COLOR); // Read the file
-	if (image.empty()) // Check for invalid input
-	{
-		cout << "Could not open or find the image" << std::endl;
-		return -1;
-	}
-	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-	imshow("Display window", image); // Show our image inside it.
-	waitKey(0); // Wait for a keystroke in the window
+	Mat defaultImg = imread(FOLDER + "01default.jpg");
+
+	Mat grayscaleImg = GetGrayscaleImg(defaultImg);
+	imshow("Grayscale image", grayscaleImg);
+	waitKey();
+	imwrite(FOLDER + "02grayscale.jpg", grayscaleImg);
+
 	return 0;
 }
